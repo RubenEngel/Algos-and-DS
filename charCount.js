64bccfd69931da1b3f4cf1463f82e915ef2ff3b5
Ruben Engel <ruben.engel98@gmail.com>
2021-04-30 11:27:44 +0100

    Update charCount.js

--- charCount.js
+++ charCount.js
@@ -1,15 +1,70 @@
-function charCount(str) {
-    var result = {};
-    for (var i = 0; i < str.length; i++) {
-        var char = str[i].toLowerCase()
-        if (result[char]>0) {
-            result[char]++;
+// function charCount(str) {
+//     var result = {};
+//     for (var i = 0; i < str.length; i++) {
+//         var char = str[i].toLowerCase()
+//         if (result[char]>0) {
+//             result[char]++;
+//         }
+//         else {
+//             result[char]=1;
+//         }
+//     }
+//     return result
+// }
+
+// function charCount(str) {
+//     var obj = {};
+//     for (var char of str) {
+//         var char = char.toLowerCase()
+//         if (/[a-z0-9]/.test(char)) {
+//             obj[char] = ++obj[char] || 1;
+//         }
+//     }
+//     return obj;
+// }
+
+// this is faster than using RegExp
+function isAlphaNumeric(char) {
+    var code = char.charCodeAt(0)
+    if ((code > 47 && code < 58) ||
+        (code > 64 && code < 91) ||
+        (code > 96 && code < 123)) {
+            return true;
         }
-        else {
-            result[char]=1;
+    return false;
+}
+
+// function charCount(str) {
+//     var obj = {};
+//     for (var char of str) {
+//         if (isAlphaNumeric(char)) {
+//             var char = char.toLowerCase()
+//             obj[char] = ++obj[char] || 1;
+//         }
+//     }
+//     return obj;
+// }
+
+// function charCount(str) {
+//     var obj = {};
+//     for (var char of str) {
+//         if (isAlphaNumeric(char)) {
+//             var char = char.toLowerCase()
+//             obj[char] = (obj[char] || 0) + 1;
+//         }
+//     }
+//     return obj;
+// }
+
+function charCount(str) {
+    var obj = {};
+    for (var char of str) {
+        var char = char.toLowerCase()
+        if (/[a-z0-9]/.test(char)) {
+            obj[char] = obj[char] ? ++obj[char] : 1
         }
     }
-    return result
+    return obj;
 }
 
 input = "Hello, testing, testing 123"
