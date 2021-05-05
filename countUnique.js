d73c9a72c07aa2d8a6a8e46758590cebb937c3f8
Ruben Engel <ruben.engel98@gmail.com>
2021-05-05 14:50:34 +0100

    Update countUnique.js

--- countUnique.js
+++ countUnique.js
@@ -14,23 +14,49 @@
 //     return unique_values.length
 // }
 
-// own solution with O(1)
+// own solution with O(1) space comp using while loop
+// function countUniqueValues(input_array) {
+//     let i = 0;
+//     let j = i+1;
+//     let unique_values_count = input_array.length ? 1 : 0;
+//     while (j < input_array.length) {
+//         if (input_array[i] === input_array[j]) {
+//             ++j;
+//         } else {
+//             // unique_values.push(input_array[j])
+//             ++unique_values_count;
+//             i = j;
+//             ++j;
+//         }
+//     }
+//     return unique_values_count;
+// }
+
+// own solution with O(1) comp using while loop
 function countUniqueValues(input_array) {
     let i = 0;
-    let j = i+1;
     let unique_values_count = input_array.length ? 1 : 0;
-    while (j < input_array.length) {
-        if (input_array[i] === input_array[j]) {
-            ++j;
-        } else {
-            // unique_values.push(input_array[j])
+    for (let j = 1; j < input_array.length; j++) {
+        if (input_array[i] !== input_array[j]) {
             ++unique_values_count;
             i = j;
-            ++j;
         }
     }
     return unique_values_count;
 }
+
+// colt solution
+// function countUniqueValues(input_array) {
+//     if (input_array.length === 0) return 0;
+//     let i = 0;
+//     for (let j=1; j < input_array.length; j++) {
+//         if (input_array[i] !== input_array[j]) {
+//             i++;
+//             input_array[i] = input_array[j];
+//         }
+//     }
+//     return i+1;
+// }
 
 
 let input = [-2, -2, -1, -1, 0, 1, 2, 2, 3, 4, 4, 4, 5, 5, 6];
